a09fd7017373f11865cd2eb6331eaf4809d0f561
Sven Marquardt <sven2q@hotmail.de>
2018-05-21 19:44:16 +0200

    Get the artifact as dependencie for pom
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

--- jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/MavenArtifact.java
+++ jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/MavenArtifact.java
@@ -8,6 +8,21 @@ public class MavenArtifact {
 
 	private final String version;
 
+	private final static String ARTIFACT = "<artifactId>";
+
+	private final static String ARTIFACT_CLOSE = "</artifactId>";
+
+	private final static String GROUPID = "<groupId>";
+
+	private final static String GROUPID_CLOSE = "</groupId>";
+
+	private final static String VERSION = "<version>";
+
+	private final static String VERSION_CLOSE = "</version>";
+
+	private final static String DEPENDENCIE = "<dependencie>";
+	private final static String DEPENDENCIE_CLOSE = "</dependencie>";
+
 	/**
 	 * @param artifactId
 	 * @param groupId
@@ -20,25 +35,11 @@ public class MavenArtifact {
 		this.version = version;
 	}
 
-	/**
-	 * @return the artifactId
-	 */
-	public String getArtifactId() {
-		return this.artifactId;
-	}
-
-	/**
-	 * @return the groupId
-	 */
-	public String getGroupId() {
-		return this.groupId;
-	}
-
-	/**
-	 * @return the version
-	 */
-	public String getVersion() {
-		return this.version;
+	public String getAsDependencie() {
+		return new StringBuilder().append(MavenArtifact.DEPENDENCIE).append(this.groupId).append(this.groupId)
+				.append(MavenArtifact.GROUPID_CLOSE).append(MavenArtifact.ARTIFACT).append(this.artifactId)
+				.append(MavenArtifact.ARTIFACT_CLOSE).append(MavenArtifact.VERSION).append(this.version)
+				.append(MavenArtifact.VERSION_CLOSE).append(MavenArtifact.DEPENDENCIE_CLOSE).toString();
 	}
 
 }
