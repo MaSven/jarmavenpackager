94daed43f7a6cfb0936602cf8f34c01dce4f0424
Sven Marquardt <sven2q@hotmail.de>
2018-05-21 02:17:19 +0200

    Added doc
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/impl/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Implementations of the
+ * {@link space.smarquardt.jarmavenpackager.dependencieserver.IDependencieServer}.
+ *
+ * @author sven
+ *
+ */
+package space.smarquardt.jarmavenpackager.dependencieserver.impl;
--- jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/package-info.java
+++ jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/package-info.java
@@ -1,7 +1,7 @@
+
 /**
+ * Contains the implementations for all the various Dependencie server out there
  * 
- */
-/**
  * @author sven
  *
  */
