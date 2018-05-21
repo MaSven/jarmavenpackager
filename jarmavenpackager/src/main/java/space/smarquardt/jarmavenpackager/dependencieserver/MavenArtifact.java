1b37277e5aaf919eacfa6b3c49fe05b2a41f38c3
Sven Marquardt <sven2q@hotmail.de>
2018-05-21 02:46:33 +0200

    Class to hold the information of the retrieved artifacts
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ jarmavenpackager/src/main/java/space/smarquardt/jarmavenpackager/dependencieserver/MavenArtifact.java
@@ -0,0 +1,15 @@
+package space.smarquardt.jarmavenpackager.dependencieserver;
+
+public class MavenArtifact {
+
+	private String artifactId;
+
+	private String groupId;
+
+	private String version;
+
+	public MavenArtifact() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
